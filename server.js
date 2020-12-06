0f6695f47dd4600bde6c53d588361accd0de504d
JoseSachango <62193290+JoseSachango@users.noreply.github.com>
2020-12-06 11:49:40 -0600

    Initial deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path  = require("path")
+
+var app = express()
+
+
+var PORT = process.env.PORT || 3003;
+
+//middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static("public"))
+
+
+//html Routes
+app.get("/",function(request,response){
+    response.sendFile(path.join(__dirname,"/public/index.html"))
+});
