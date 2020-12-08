bfb3ff5d34955492afae9c122b0bbb927757bf36
JoseSachango <62193290+JoseSachango@users.noreply.github.com>
2020-12-08 14:36:59 -0600

    Commiting newest changes

--- public/notes.js
+++ public/notes.js
@@ -1,6 +1,30 @@
 
 $(document).ready(function(){
 
+    console.log("This is the local storage length: ")
+    console.log(localStorage.length)
+
+    var emptyNoteCol = 
+            `<div class="card">
+            <ul class="list-group">
+            </ul>
+        </div>`
+    
+    
+    
+    //localStorage.clear()
+    if(localStorage.length===0||localStorage.getItem("noteColHtml")===emptyNoteCol){
+        $("ul").append(`<li id="startingLi" class="list-group-item">No Saved Notes</li>`)
+    }else{
+
+        $(".col-4").html(localStorage.getItem("noteColHtml"))
+    }
+
+        
+
+    
+
+    
 
     //Showing the save button when the user inputs to the text area field.
     $("#noteTextArea").on("input",function(){
@@ -8,9 +32,9 @@ $(document).ready(function(){
         $("#saveNote").show()
     
     })
+    //---
     
     //Sending a post request to the /api/notes endpoint
-    
     $("#saveNote").on("click",function(){
         
         var randomNumber = Math.floor(Math.random()*1000)
@@ -28,9 +52,17 @@ $(document).ready(function(){
        
         //appending new note to the unordered list
         var noteHtml = `<li  id="${uniqueId}li" class="list-group-item">${$("#noteTitle").val().trim()}<i id="${uniqueId}" class="fa fa-trash float-right" style="color:red"></i></li>`
-        
+        $("#startingLi").remove()
         $("ul").append(noteHtml)
+        localStorage.setItem("noteColHtml",$(".col-4").html())
         //----
+
+        //saving list item to localstorage
+        localStorage.setItem(`${$("#noteTitle").val().trim()}`,$("#noteTextArea").val().trim())
+
+        console.log("The container that holds the lists: ")
+        console.log(localStorage.getItem(`${$("#noteTitle").val().trim()}`))
+        //--
     
         $.post("/api/notes",noteData).then((response)=>{
             console.log(response)
@@ -73,11 +105,14 @@ $(document).ready(function(){
                 //make a get request to /api/notes to get the array of note objects. 
             if (event.target.getAttribute("class")==="fa fa-trash float-right" ){
 
-                console.log("The id of the event is the following : ")
-                console.log(event.target.getAttribute("id"))
+                console.log("The event.target.parentNode.childNodes[0].data is the following : ")
+                console.log(event.target.parentNode.childNodes[0].data)
+                //--
+                localStorage.removeItem(event.target.parentNode.childNodes[0].data)
+                
+                //--
 
-                //removing list item from the client
-                $(`#${event.target.getAttribute("id")}li`).remove()
+               
 
                 $.ajax(
                     {
@@ -93,11 +128,43 @@ $(document).ready(function(){
                 }).catch((err)=>{
                     console.log(err)
                 })
+
                 
-            }else{
+               
+                
+            }
+
+            else if(event.target.getAttribute("class")==="list-group-item"){
+
+                /*
+                var listItemId = event.target.getAttribute("id")
+                console.log("Target html: ")
+                console.log(event.target.childNodes[0].data)
+
+                console.log("Target id: ")
+                console.log(listItemId)
+                console.log(localStorage.getItem(listItemId))
+                */
+
+                $("#noteTitle").val(event.target.childNodes[0].data)
+                $("#noteTextArea").val(localStorage.getItem(event.target.childNodes[0].data)                                             )
+
+            }
+            
+            else{
                 
                 console.log(event.target.getAttribute("class"))
             }
+
+             //removing list item from the client
+             $(`#${event.target.getAttribute("id")}li`).remove()
+             localStorage.setItem("noteColHtml",$(".col-4").html())
+
+             console.log("What is this below")
+             console.log(`#${event.target.getAttribute("id")}li`)
+             console.log(`#${event.target.getAttribute("id")}`)
+
+            
 
     }) 
 
